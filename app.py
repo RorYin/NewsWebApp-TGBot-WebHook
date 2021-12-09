7c956a2ce1427c19456b9da909bd897ad75d1702
Dhanush N <71968114+RorYin@users.noreply.github.com>
2021-12-09 20:53:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,12 +15,18 @@ bot.set_webhook(url=url)
 app = Flask(__name__)
 @app.route('/',methods=['POST'])
 def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    bot.process_new_updates([update])
-    return ('ok',200)
+    try:
+        update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+        bot.process_new_updates([update])
+        return ('ok',200)
+    except Exception as e:
+        print("error is : ",e)
 
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    bot.send_message(msg.chat.id,"Hello user")
+    try:
+        bot.send_message(msg.chat.id,"Hello user")
+    except Exception as e:
+        print("Error is : ",e)
 
