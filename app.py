447f3bb14d1cfbdd15d7922612e1184bab7ef1db
Dhanush N <71968114+RorYin@users.noreply.github.com>
2021-12-09 20:43:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask.wrappers import Request
+import telebot
+from flask import Flask, json ,request
+
+token="5002606701:AAFt2QcHWaEgqW0T63dXmTIQYglYX2Dw1bM"
+secret=""
+url="https://roryin.pythonanywhere.com/"
+
+bot = telebot.TeleBot(token,threaded=False)
+bot.remove_webhook()
+bot.set_webhook(url=url)
+
+
+app = Flask(__name__)
+@app.route('/',methods=['POST'])
+def webhook():
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return ('ok',200)
+
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    bot.send_message(msg.chat.id,"Hello user")
+
